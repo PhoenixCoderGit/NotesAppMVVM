b4d9e0d54126fb85f174d35a1f91dd263c8c65dd
felix <ingcodefenix@gmail.com>
2023-08-27 15:27:51 -0400

    leccion2

--- app/src/main/java/com/felixmamaniquispe/notesappmvvm/navigation/NotesNavHost.kt
+++ app/src/main/java/com/felixmamaniquispe/notesappmvvm/navigation/NotesNavHost.kt
@@ -4,10 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.felixmamaniquispe.notesappmvvm.screens.Add
-import com.felixmamaniquispe.notesappmvvm.screens.Main
-import com.felixmamaniquispe.notesappmvvm.screens.Note
-import com.felixmamaniquispe.notesappmvvm.screens.Start
+import com.felixmamaniquispe.notesappmvvm.screens.AddScrean
+import com.felixmamaniquispe.notesappmvvm.screens.MainScrean
+import com.felixmamaniquispe.notesappmvvm.screens.NoteScrean
+import com.felixmamaniquispe.notesappmvvm.screens.StartScrean
 
 sealed class NavRoute(val route: String){
     object Start: NavRoute("start_screen")
@@ -18,12 +18,26 @@ sealed class NavRoute(val route: String){
 
 @Composable
 fun NotesNavHost (){
+
+    // creando un objeto de navegacion
     val navController = rememberNavController()
     
-    NavHost(navController = navController, startDestination = NavRoute.Start.route){
-        composable(NavRoute.Start.route){Start(navController = navController)}
-        composable(NavRoute.Main.route){Main(navController = navController)}
-        composable(NavRoute.Add.route){Add(navController = navController)}
-        composable(NavRoute.Note.route){Note(navController = navController)}
+    NavHost(
+        navController = navController,
+        startDestination = NavRoute.Start.route
+    ){
+        composable(NavRoute.Start.route){
+            StartScrean(navController = navController)
+        }
+        composable(NavRoute.Main.route){
+            MainScrean(navController = navController)
+        }
+        composable(NavRoute.Add.route){
+            AddScrean(navController = navController)
+        }
+        composable(NavRoute.Note.route){
+            NoteScrean(navController = navController)
+        }
     }
+
 }
--- app/src/main/java/com/felixmamaniquispe/notesappmvvm/screens/Add.kt
+++ app/src/main/java/com/felixmamaniquispe/notesappmvvm/screens/Add.kt
@@ -1,8 +1,70 @@
 package com.felixmamaniquispe.notesappmvvm.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.felixmamaniquispe.notesappmvvm.navigation.NavRoute
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Add(navController: NavHostController) {
+fun AddScrean(navController: NavHostController) {
+
+    var title by remember { mutableStateOf("")}
+    var subtitle by remember { mutableStateOf("")}
+
+    Scaffold(
+        modifier = Modifier
+            //.fillMaxSize()
+            .padding(top = 40.dp),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Agregar",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+            OutlinedTextField(
+                value = title,
+                onValueChange = {title = it},
+                label = { Text(text = "Nota titulo")}
+            )
+            OutlinedTextField(
+                value = subtitle,
+                onValueChange = {subtitle = it},
+                label = { Text(text = "Nota subtitulo")}
+            )
+            Button(
+                modifier = Modifier.padding(top = 16.dp),
+                onClick = {
+                    navController.navigate(NavRoute.Main.route)
+                }
+            ) {
+                Text(text = "Agregar nota")
+            }
+        }
+    }
 }

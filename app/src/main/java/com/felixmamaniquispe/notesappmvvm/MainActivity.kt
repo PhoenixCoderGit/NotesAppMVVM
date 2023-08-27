d173b94e81d42f8293b5448840b84cd46f9dfe8b
felix <ingcodefenix@gmail.com>
2023-08-27 01:14:00 -0400

    leccion1

--- app/src/main/java/com/felixmamaniquispe/notesappmvvm/MainActivity.kt
+++ app/src/main/java/com/felixmamaniquispe/notesappmvvm/MainActivity.kt
@@ -6,18 +6,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.felixmamaniquispe.notesappmvvm.navigation.NotesNavHost
 import com.felixmamaniquispe.notesappmvvm.ui.theme.NotesAppMVVMTheme
 
@@ -32,18 +38,28 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     topBar = {
                         TopAppBar(
+                            navigationIcon = {
+                               IconButton(onClick = { /*TODO*/ }) {
+                                   Icon(
+                                       imageVector = Icons.Default.Menu,
+                                       contentDescription = "Menu Icon"
+                                   )
+                               }
+                            },
                             title = {
                                 Text(text = "Notas App")
                             },
-                            Modifier.background(Color.Blue),
+                            colors = TopAppBarDefaults.smallTopAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+                            )
                         )
                     },
                     content = {
                         Surface(
                             modifier = Modifier.fillMaxSize(),
-
+                            color= MaterialTheme.colorScheme.background
                         ) {
-
+                            NotesNavHost()
                         }
                     }
                 )
